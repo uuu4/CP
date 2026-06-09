fbad84b05f4766b22adb41523a4ae2bab454c24c
Ali Emre Aydın <alinintopsecreti@gmail.com>
2026-06-09 20:29:19 +0300

    second at AtCoder

---
+++ atcoder/Armor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int A,D;
+    cin >>A >> D;
+    if(D>=A){
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
+
+
+    return 0;
+}
