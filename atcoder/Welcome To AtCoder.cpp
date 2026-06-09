d3b3f7582273a0999eed4ee37f1d475b512cea46
Ali Emre Aydın <alinintopsecreti@gmail.com>
2026-06-09 20:19:52 +0300

    first in atcoder

---
+++ atcoder/Welcome To AtCoder.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int a,b,c;
+    string s;
+    cin >> a;
+    cin >> b >> c;
+    cin >> s;
+
+    cout << a+b+c << " " << s;
+    
+    
+    return 0;}
