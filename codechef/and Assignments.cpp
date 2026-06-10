e32b0be56252dda8151dbdbc0abcddb1499a0a65
Ali Emre Aydın <alinintopsecreti@gmail.com>
2026-06-10 23:00:38 +0300

    hmmm

---
+++ codechef/and Assignments.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int x;
+	cin >> x;
+	
+	if(x<=7){
+	    cout << "YES" << "\n";
+	}
+	else{
+	cout << "NO" << "\n";}
+}
+int main() {
+    int t;
+    cin >> t;
+    while(t>0){
+        solve();
+        t--;
+    }
+	
+    return 0;
+}
