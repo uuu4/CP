4f85b6cb8cc7282fdd4a59ed65de063fd3e2d267
Ali Emre Aydın <alinintopsecreti@gmail.com>
2026-06-10 23:07:21 +0300

    keske boyle hayatim olsa

---
+++ codechef/Chef in his Office.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve(){
+    int x,y;
+    cin >> x >> y;
+    
+    cout << 4*x+y<<"\n";
+}
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    
+	return 0;
+
+}
