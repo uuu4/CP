e6da62c7502e56fe7a352777ed7ad6d02967831d
Ali Emre Aydın <alinintopsecreti@gmail.com>
2026-06-11 12:37:15 +0300

    codechef

---
+++ codechef/Chefland Games.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	int r1,r2,r3,r4;
+	cin >> r1 >> r2 >> r3 >> r4;
+	
+	if(r1==0&&r2==0&&r3==0&&r4==0) cout << "in" << "\n";
+	else cout << "out" << "\n";}
+int main() {
+    
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
