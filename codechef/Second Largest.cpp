b2ca9902dcdf39629965145f465f9d813a024ecd
Ali Emre Aydın <alinintopsecreti@gmail.com>
2026-06-12 01:40:05 +0300

    hey

---
+++ codechef/Second Largest.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+void solve(){int arr[3];
+	cin >> arr[0] >> arr[1] >> arr[2];
+	
+	sort(arr,arr+3);
+	cout << arr[1] << "\n";}
+	
+int main() {
+    int t;
+    cin >> t;
+    while(t--)solve();
+	
+
+}
