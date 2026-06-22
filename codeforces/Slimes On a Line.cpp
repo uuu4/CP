e6098df30271551eb99b18aafbcc91aa9dd49622
Ali Emre Aydın <alinintopsecreti@gmail.com>
2026-06-23 00:06:01 +0300

    another one

--- codeforces/Slimes On a Line.cpp
+++ codeforces/Slimes On a Line.cpp
@@ -1,25 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+void solve(){
     int n;
     cin >> n;
-    vector<int> x(n);
-    int sum=0;
-    for(int i=0;i<n;i++){
-        cin >> x[i];
-        sum+=x[i];
-    }
-    sort(x.begin(),x.end());
-    double avg = double(sum/n);
-    if(abs(avg-x[0])>=abs(avg-x[n-1])){
-        cout << abs(avg-x[0]);
-    }
-    else {
-        cout << abs(avg-x[n-1]);
-    }
 
+    vector<int> a(n);
+    for (int &x : a) cin >> x;
 
+    int mx = *max_element(a.begin(), a.end());
+    int mn = *min_element(a.begin(), a.end());
+    cout << (mx - mn + 1) / 2 << "\n";
+
+}
+int main(){
+    int q;
+    cin >> q;
+    while(q--)solve();
 
 
     return 0;
